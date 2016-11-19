156c91bd4c1993b5ba2af42b100e77c02177c12c
Ignacio Ojanguren <ignacioojanguren@MacBook-Pro-de-Ignacio.local>
2016-11-19 11:41:19 -0500

    Added comments
    Comments explaining the functionality were added.

--- Calculation/Probabilities.java
+++ Calculation/Probabilities.java
@@ -9,6 +9,18 @@ import Classes.Team;
  */
 public class Probabilities {
 
+    /**
+     * This method calculates the probability a player has to score.
+     * It is not a very accurate calculation, as the percentage is only calculated given the total goals of the
+     * team how many goals has that player scored.
+     * This is not very accurate, because even if a player hasn't scored, the player has a probability to score a goal.
+     * @param goalsPlayer
+     *  This class contains the goals of a player and the name of the player that scored the goals.
+     * @param team
+     *  The class team contains all the goals that team has scored.
+     * @return
+     *  Returns a doube with the probability a player has to score.
+     */
     public static double calculatePlayerGoalsInTeam(GoalsPlayer goalsPlayer, Team team){
         double probability = 0;
         int minutesPlayed = goalsPlayer.getTimePlayed(), numberGoals = goalsPlayer.getGoals(), goalsTeam = team.getGoalsFor();
